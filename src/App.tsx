0f4517d0c81682ed9ab036a26747f8fb9e208bd7
Ringo Hoffmann <riho@live.de>
2021-08-22 12:07:38 +0200

    add styled plugin; add global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import {BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle``;
 
 function App() {
   return (
     <div>
       <Router></Router>
+      <GlobalStyle />
     </div>
   );
 }
